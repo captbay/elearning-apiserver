00cb1b076552b38ed3fe0e1f2d907abeacb859f3
captbay <agespramana9@gmail.com>
2022-11-20 01:57:56 +0700

    master

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,7 +36,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->resource('login');
+$routes->post('login', 'Login::create');
 $routes->resource('kelas');
 $routes->resource('todolists');
 $routes->resource('users');
